65765d2a800cbd2c03dbcc4d05cd797edc7fc52f
Anna Pavlenko <annapavlenko637@gmail.com>
2024-02-05 09:25:56 +0200

    1 commit

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,71 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Інтернет Магазин</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/starter-template.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">Інтернет Магазин</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li ><a href="/">Всі товари</a></li>
+                <li  class="active" ><a href="categories">Категорії</a>
+                </li>
+                <li ><a href="basket">В корзину</a></li>
+                <li><a href="reset">Скинути проект у початковий стан</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="admin/home">Панель Адміністратора</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<div class="container">
+    <div class="starter-template">
+        <div class="panel">
+            <a href="mobiles">
+                <img src="storage/categories/mobile.jpg">
+                <h2>Мобільні телефони</h2>
+            </a>
+            <p>
+                У цьому розділі ви знайдете найпопулярніші мобільні телефони за чудовими цінами!
+            </p>
+        </div>
+        <div class="panel">
+            <a href="portable">
+                <img src="storage/categories/portable.jpg">
+                <h2>Портативна техніка</h2>
+            </a>
+            <p>
+                Розділ із портативною технікою.
+            </p>
+        </div>
+        <div class="panel">
+            <a href="appliances">
+                <img src="storage/categories/appliance.jpg">
+                <h2>Побутова техніка</h2>
+            </a>
+            <p>
+                Розділ із побутовою технікою
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,49 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Інтернет Магазин</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/starter-template.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">Інтернет Магазин</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li ><a href="/">Всі товари</a></li>
+                <li  class="active" ><a href="categories">Категорії</a>
+                </li>
+                <li ><a href="basket">В корзину</a></li>
+                <li><a href="reset">Скинути проект у початковий стан</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="admin/home">Панель Адміністратора</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<div class="container">
+    <div class="starter-template">
+        <h1>iPhone X 64GB</h1>
+        <p>Цена: <b>70 000 грн.</b></p>
+        <img src="#/storage/products/iphone_x.jpg">
+        <p>Відмінний  телефон з пам'яттю на 64 GB</p>
+        <a class="btn btn-success" href="#/basket/1/add">Додати в корзину</a>
+    </div>
+</div>
+</body>
+</html>
